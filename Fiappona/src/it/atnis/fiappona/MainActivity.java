7f00ff2cf3278b692bb1d496251afd0849b9a284
ivano <ivano.agosta@atnis.it>
2012-08-06 12:17:37 +0200

    v0.1

--- Fiappona/src/it/atnis/fiappona/MainActivity.java
+++ Fiappona/src/it/atnis/fiappona/MainActivity.java
@@ -1,17 +1,19 @@
 package it.atnis.fiappona;
 
+import org.apache.cordova.DroidGap;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 
-public class MainActivity extends Activity {
+public class MainActivity extends DroidGap {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.loadUrl("file://android_asset/www/index.html");
     }
 
     @Override
